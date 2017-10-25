a7540522ba657c01692186732b1b3ace408a0957
Supun Dissanayake <supunlakshandissanayake@gmail.com>
2017-10-25 22:36:06 +0530

    added db config files

--- src/main/java/com/xfinity/db/DbConnectionProvider.java
+++ src/main/java/com/xfinity/db/DbConnectionProvider.java
@@ -1,13 +1,27 @@
 package com.xfinity.db;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class DbConnectionProvider {//2.707949508
+public class DbConnectionProvider {
 
     public static Connection getDbConnection() throws SQLException, ClassNotFoundException {
+        Properties props = new Properties();
+        try (FileInputStream savedProperties = new FileInputStream("application-properties.conf")) {
+            props.load(savedProperties);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(DbConnectionProvider.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(DbConnectionProvider.class.getName()).log(Level.SEVERE, null, ex);
+        }
         Class.forName("org.mariadb.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/medilab", "root", "1234");
+        return DriverManager.getConnection(String.format("jdbc:mysql://%s:%s/%s", props.getProperty("host"), props.getProperty("port"), props.getProperty("db")), props.getProperty("user"), props.getProperty("password"));
     }
 }
